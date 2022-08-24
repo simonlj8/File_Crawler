946f4a174cb9a481a31019d5d164882bd4b8530a
Simon Ljungström <simonlj8@gmail.com>
2022-08-24 16:12:10 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println(sc.nextLine());
+
+
+    }
+
+}
