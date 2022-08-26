bd86bebb96ad63229e97d015faf8d41e56f7be49
Simon Ljungström <simonlj8@gmail.com>
2022-08-26 16:51:52 +0200

    I't can read files!

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,11 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Write the filename you want to search!");
-
-
+       /* System.out.println("Write the filename you want to search!");
         Scanner sc = new Scanner(System.in);
+        System.out.println("You want to search for the file " + "" + sc.nextLine());*/
 
-        System.out.println("You want to search for the file " + "" + sc.nextLine());
-
-        readFile("c:\\temp\\text.tx");
+        readFile("c:\\temp\\text.txt");
 
 
     }
@@ -25,7 +22,13 @@ public class Main {
 
         File file = new File(filePath);
         try {
-            FileReader fileReader = new FileReader(file);
+        //    FileReader fileReader = new FileReader(file);
+
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
         } catch (FileNotFoundException e) {
             System.out.println("File don't exist");
         }
