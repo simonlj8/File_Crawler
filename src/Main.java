065a45dcc1573e096510c489b4968e8c4f683817
Simon Ljungström <simonlj8@gmail.com>
2022-08-26 20:23:07 +0200

    I't can write files!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.Scanner;
-
+import java.io.IOException;
 
 
 public class Main {
@@ -13,24 +11,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("You want to search for the file " + "" + sc.nextLine());*/
 
-        readFile("c:\\temp\\text.txt");
+       // readFile("c:\\temp\\text.txt");
+
+        writeFile("c:\\temp\\text2.txt");
 
 
+    }
+
+    private static void writeFile(String filePath) {
+
+        try  (FileWriter fileWriter = new FileWriter(filePath)) {
+
+            fileWriter.write("Dagens nyheter: En kanot välte i sjön Gautan nedanför Tärnaby Fjällhotell" +
+                    " vid 11-tiden under fredagen. Två personer från San Francisco, som är på besök i Tärnaby" +
+                    " för att delta vid ett bröllop under helgen, hamnade i vattnet.");
+        } catch (IOException e) {
+            System.out.println("Blev inget");
+            throw new RuntimeException(e);
+
+        }
     }
 
     private static void readFile(String filePath) {
 
         File file = new File(filePath);
+
+
+        Scanner scanner = null;
         try {
         //    FileReader fileReader = new FileReader(file);
 
-            Scanner scanner = new Scanner(file);
+            scanner = new Scanner(file);
 
             while (scanner.hasNext()) {
                 System.out.println(scanner.nextLine());
             }
         } catch (FileNotFoundException e) {
             System.out.println("File don't exist");
+            throw new RuntimeException(e);
+
+        } finally {
+            if(scanner != null);
+            scanner.close();
         }
     }
 
