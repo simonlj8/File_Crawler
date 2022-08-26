4f4039b55673d77a271e7ec3f458f399dc9ea959
Simon Ljungström <simonlj8@gmail.com>
2022-08-26 16:29:46 +0200

    Trying to commit & push via IntelliJ

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Scanner;
+
+
+
 public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello World");
+        System.out.println("Write the filename you want to search!");
 
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("You want to search for the file " + "" + sc.nextLine());
 
-        System.out.println(sc.nextLine());
+        readFile("c:\\temp\\text.tx");
 
 
+    }
+
+    private static void readFile(String filePath) {
+
+        File file = new File(filePath);
+        try {
+            FileReader fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("File don't exist");
+        }
     }
 
 }
